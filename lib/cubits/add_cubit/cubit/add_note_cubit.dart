8ba1a558da5ac24e3547a683bb1b68dd59ff8927
Ahmed Elsawy <86520788+A-Elsawy@users.noreply.github.com>
2024-01-27 20:13:07 +0200

    add cubit to bottom sheet👍

--- lib/cubits/add_cubit/cubit/add_note_cubit.dart
+++ lib/cubits/add_cubit/cubit/add_note_cubit.dart
@@ -5,13 +5,14 @@ import 'package:notes_app/models/note_model.dart';
 
 part 'add_note_cubit_state.dart';
 
-class AddNoteCubit extends Cubit<AddNoteCubitState> {
+class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
 
   void addNote(NoteModel noteModel) async {
     emit(AddNoteLoading());
     try {
       Box notesBox = Hive.box('notes_box');
+
       emit(AddNoteSuccess());
       await notesBox.add(noteModel);
     } catch (e) {
--- lib/widgets/custom_bottom_sheet.dart
+++ lib/widgets/custom_bottom_sheet.dart
@@ -1,4 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notes_app/cubits/add_cubit/cubit/add_note_cubit.dart';
 import 'package:notes_app/widgets/add_note_form.dart';
 
 class AddCustomNoteBottomSheet extends StatelessWidget {
@@ -6,11 +11,27 @@ class AddCustomNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: AddNoteForm(),
+        physics: const BouncingScrollPhysics(),
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNoteFailure) {
+              log('Failed ${state.errMessage}');
+            }
+
+            if (state is AddNoteSuccess) {
+              Navigator.pop(context);
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoading ? true : false,
+              child: const AddNoteForm(),
+            );
+          },
+        ),
       ),
     );
   }
